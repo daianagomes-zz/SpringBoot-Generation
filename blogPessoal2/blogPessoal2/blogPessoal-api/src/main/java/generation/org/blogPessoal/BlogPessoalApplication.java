fb567a8410d39c711a35e3d3f8ecd28597ec9624
unknown <daianagomes@id.uff.br>
2020-10-07 14:21:53 -0300

    Atividade Spring 4

---
+++ blogPessoal2/blogPessoal2/blogPessoal-api/src/main/java/generation/org/blogPessoal/BlogPessoalApplication.java
@@ -0,0 +1,13 @@
+package generation.org.blogPessoal;
+
+
+@SpringBootApplication
+public class BlogPessoalApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BlogPessoalApplication.class, args);
+	}
+
+}
